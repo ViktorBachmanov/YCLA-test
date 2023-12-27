6acd38b6673c955550a816b101e09c30b76dbe4b
Viktor Bachmanov <vbachmanov@mail.ru>
2023-12-27 17:37:54 +0300

    Add 'fio' and 'phone' fields into the 'users' table

---
+++ database/migrations/2023_12_27_143434_add_fio_and_phone_fields_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('patronymic', 100)->after('name');
+            $table->string('family', 100)->after('patronymic');
+            $table->string('phone', 20)->after('family')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('patronymic');
+            $table->dropColumn('family');
+            $table->dropColumn('phone');
+        });
+    }
+};
