ec85286fe3cc11e8913febfda6e5349379060c94
Viktor Bachmanov <vbachmanov@mail.ru>
2023-12-28 14:39:15 +0300

    Create an 'Option' model with migration

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the options associated with the product.
+     */
+    public function options(): HasOne
+    {
+        return $this->hasOne(Option::class);
+    }
 }
